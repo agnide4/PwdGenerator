3b2154ec8eac3a2909b10fa8bb285e4d9d5f6348
Samir <agnide4@gmail.com>
2020-05-09 23:36:54 -0500

    Created variable in JS files

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,28 @@
+
+
+//Data needed for functions
+var arraySpc = [];
+var arrayLtr = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
+var arrayLtrLow = [];
+var arrayNbr = [0,1,2,3,4,5,6,7,8,9];
+
+
+
+//Functions for creating lower case array
+for (i= 0 ; i < arrayLtr.length; i++) {
+  arrayLtr[i].Tolowercase();
+  arrayLtrLow.push;
+  i++;
+}
+
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
