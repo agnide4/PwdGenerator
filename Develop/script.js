966df41ac7d93b2292793f9c495a1e9f5ca2834a
Samir Bello <agnide4@gmail.com>
2020-05-12 16:28:07 -0500

    Changed functions for random items

--- Develop/script.js
+++ Develop/script.js
@@ -5,62 +5,79 @@ var arraySpc = ['@', '$', '%', '&', '!', '#'];
 let arrayLtr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var arrayLtrLow = arrayLtr.toLocaleString().toLowerCase().split(',');
 var arrayNbr = [0,1,2,3,4,5,6,7,8,9];
-var pwdLength = document.querySelector("pwdlength");
-
-function arrayRandom(Array){
-    var randomItem = Array[Math.floor(Math.random()*Array.length)];
-    
-  }
-
-function generatePassword(){
-  var pwdStr;
-let i = 0;
-while (i<pwdLength){
-  if (document.getElementById("Nbr").checked = true){
-    var nRandom = arrayRandom(arrayNbr);
-    pwdStr += nRandom;
-
-  }
-  if (document.getElementById("Lwr").checked = true){
-    var lRandom = arrayRandom(arrayLtrLow);
-    pwdStr += lRandom;
-  }
-
-  if (document.getElementById("Cpl").checked = true){
-    var cRandom = arrayRandom(arrayLtr);
-    pwdStr += cRandom;
-  }
-  if (document.getElementById("Spcl").checked = true){
-    var sRandom = arrayRandom(arraySpc);
-    pwdStr += sRandom;
-  }
-}
-return pwdStr;
-
-}
-
-
-
-
-
-
-
-
-
-
-
-// Assignment Code
+var pwdLength = document.querySelector("#pwdlength");
 var generateBtn = document.querySelector("#generate");
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword());
 
-  passwordText.value = password;
+
+
+//functions to get random item from each array
+
+function spcRandom(){
+    var randomItem = arraySpc[Math.floor(Math.random()*arraySpc.length)];
+    return randomItem;
+    
+  }
+  function ltrRandom(){
+    var randomItem = arrayLtr[Math.floor(Math.random()*arrayLtr.length)];
+    return randomItem;
+    
+  }
+  function lowRandom(){
+    var randomItem = arrayLtrLow[Math.floor(Math.random()*arrayLtr.length)];
+    return randomItem;
+  }
+
+  function nbrRandom(){
+    var randomItem = arrayNbr[Math.floor(Math.random()*arrayLtr.length)];
+    return randomItem;
+  }
+  
+
+function generatePassword(){
+var pwdStr;
+let i = 0;
+while (i<pwdLength){
+  if (document.getElementById("Nbr").checked == true){
+    var nRandom = nbrRandom();
+    pwdStr += nRandom;
+
+  }
+  if (document.getElementById("Lwr").checked == true){
+    var lRandom = lowRandom();
+    pwdStr += lRandom;
+  }
+
+  if (document.getElementById("Cpl").checked == true){
+    var cRandom = ltrRandom();
+    pwdStr += cRandom;
+  }
+  if (document.getElementById("Spcl").checked == true){
+    var sRandom = spcRandom;
+    pwdStr += sRandom;
+  }
+  
+  
+}
+return pwdStr;
+
+
+
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+//Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.textContent = password;
+
+  
+
+}
+
+
